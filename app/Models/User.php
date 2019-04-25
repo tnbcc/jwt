a4f50eb2846f3dc59bec4cde5c69029fed4c1a2c
tnbcc <tniub.cc@gmail.com>
2019-04-25 04:03:41 +0000

    init

---
+++ app/Models/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+
+    protected $fillable = [
+        'name', 'password',
+    ];
+
+
+    protected $hidden = [
+        'password'
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+}
